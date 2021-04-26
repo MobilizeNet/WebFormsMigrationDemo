d07ebd1d43f8cf8305fa9e7d4e2e726258495a1b
Orellabac <orellabac@gmail.com>
2021-04-26 15:40:14 -0600

    Update attach_db.sh

--- .devcontainer/attach_db.sh
+++ .devcontainer/attach_db.sh
@@ -1,3 +1,6 @@
-sleep 15s
+sleep 30s
+echo "Creating the HiringDb"
+echo "Starting..."
 /opt/mssql-tools/bin/sqlcmd -S . -U sa -P Password1$ \
 -Q "CREATE DATABASE [HiringDb] ON (FILENAME ='/db/HiringDb.mdf'),(FILENAME = '/db/HiringDb_log.ldf') FOR ATTACH"
+echo "Done creating HiringDb"
